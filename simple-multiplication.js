35c7ae97c18574dc1b6947edb524169ce2eee32f
UmtHym <120595657+UmtHym@users.noreply.github.com>
2023-12-24 20:31:57 +0100

    Simple-multiplication

---
+++ simple-multiplication.js
@@ -0,0 +1,9 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+    let multiply = 0  
+    if (number %2 === 0){
+       return multiply = number * 8
+      }
+    return multiply = number * 9
+  }
