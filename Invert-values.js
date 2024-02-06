b453c8df62c059383fd74362bcb5466f82ed0f7c
UmtHym <120595657+UmtHym@users.noreply.github.com>
2024-02-06 11:34:40 +0100

    Invert-values

---
+++ Invert-values.js
@@ -0,0 +1,9 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+let invert = (array, i) => array.map(num => num < 0 ? Math.abs(num) : num * -1);
+
