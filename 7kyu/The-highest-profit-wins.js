4048b726d82a5b64d89a634bed724acc0a2a3a45
UmtHym <120595657+UmtHym@users.noreply.github.com>
2024-04-29 16:45:20 +0200

    The-highest-profit-wins

---
+++ 7kyu/The-highest-profit-wins.js
@@ -0,0 +1,8 @@
+function minMax(arr){
+    return [Math.min(...arr), Math.max(...arr)]; 
+  }
+  
+  //input = array of at least 1 element, never Null,undefined or similar,
+  //return = min and max of the input array in array format,
+  //use Math methods to find the min and max and return as a array with min
+  //min as the first value and max as the second
