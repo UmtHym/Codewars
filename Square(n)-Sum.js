b2c305215fb9274f562847a9144e91516eb9e364
UmtHym <120595657+UmtHym@users.noreply.github.com>
2024-07-29 13:37:56 +0200

    Square(n)-Sum

---
+++ Square(n)-Sum.js
@@ -0,0 +1,7 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+function squareSum(numbers){
+    let sum = 0
+    numbers.forEach((n)=>sum += (n*n))
+    return sum
+  }
