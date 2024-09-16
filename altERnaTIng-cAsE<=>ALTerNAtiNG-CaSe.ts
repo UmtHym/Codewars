3ba60ed9ed6d7d05bdaa53ef30c93bc90830f225
UmtHym <120595657+UmtHym@users.noreply.github.com>
2024-09-16 14:16:33 +0200

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

---
+++ altERnaTIng-cAsE<=>ALTerNAtiNG-CaSe.ts
@@ -0,0 +1,23 @@
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// toAlternatingCase("hello world") === "HELLO WORLD"
+// toAlternatingCase("HELLO WORLD") === "hello world"
+// toAlternatingCase("hello WORLD") === "HELLO world"
+// toAlternatingCase("HeLLo WoRLD") === "hEllO wOrld"
+// toAlternatingCase("12345")       === "12345"                   // Non-alphabetical characters are unaffected
+// toAlternatingCase("1a2b3c4d5e")  === "1A2B3C4D5E"
+// toAlternatingCase("String.prototype.toAlternatingCase") === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+
+//Brute Force Solution
+
+export function toAlternatingCase(s: string): string {
+    let strArr: string[] = s.split('')
+    for(let i=0; i < strArr.length; i++){
+      if(strArr[i] === strArr[i].toUpperCase()){
+        strArr[i] = strArr[i].toLowerCase()
+      }else{
+         strArr[i] = strArr[i].toUpperCase() 
+        }
+    }
+      return strArr.join('')
+  }
